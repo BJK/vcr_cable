6fe8c9732ae7467d64d8f62cf1d282adf4a90815
BJK <bernie@thecoderie.com>
2014-03-19 18:21:18 -0400

    added dirty detection so that cassettes are only inserted when the cassette name changes

--- lib/vcr_cable/cassette_middleware.rb
+++ lib/vcr_cable/cassette_middleware.rb
@@ -1,11 +1,18 @@
 module VcrCable
   class CassetteMiddleware
     def self.cassette_name
-      @@cassette_name ||= 'default_cassette'
+      @@cassette_name 
     end
 
     def self.cassette_name=(name)
       @@cassette_name = name
+    end
+
+    def update_cassette_name(name)
+      return if self.class.cassette_name == name
+      
+      self.class.cassette_name= name
+      VCR.insert_cassette(name)
     end
 
     def initialize(app)
@@ -13,9 +20,7 @@ module VcrCable
     end
     
     def call(env)
-      env['QUERY_STRING'].split('&').each {|p| CassetteMiddleware.cassette_name = p.split('=').last if p.starts_with?('cassette_name=')}
-      VCR.insert_cassette(CassetteMiddleware.cassette_name)
-      puts CassetteMiddleware.cassette_name
+      env['QUERY_STRING'].split('&').each {|p| update_cassette_name(p.split('=').last) if p.starts_with?('cassette_name=')}
       @app.call(env)
     end
   end
